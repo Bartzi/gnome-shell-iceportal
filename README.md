d0f1539185ad78fa56b1339024b5553a2272a771
Christian Bartz <christian.bartz@hpi.de>
2021-11-12 20:16:30 +0100

    add first version of extension and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# ICE Portal for the Gnome Shell
+
+Do you want to see how fast your current ICE is going while working on your laptop?
+Today is your day!
+This extension for the Gnome Shell allows you to receive the current speed of your ICE at regular intervals!
+No more looking up to know how fast you are going :muscle:. 
+Who knows, maybe this extension even allows for more information in the future?
+
+## Installation
+
+Installation is quite simple:
+1. Install `curl` (I'm sorry about this dependency but I could not make requests with Soup work until now...)
+1. Copy/or checkout the repository into the following directory: `~/.local/share/gnome-shell/extensions/iceportal@bartzi.de`.
+1. reload the session (on X) or logout and login again (on Wayland)
+1. Connect to the WiFi on your ICE
+1. Open the `Extensions` Dialog and Enable the extension
+1. Enjoy!
+
+## License
+
+The code is licensed under the GPLv3.
---
+++ extension.js
@@ -0,0 +1,88 @@
+const St = imports.gi.St;
+const Gio = imports.gi.Gio;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const Panel = imports.ui.panel;
+
+const ByteArray = imports.byteArray;
+const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
+
+const url = "https://iceportal.de/api1/rs/status/";
+
+class Extension {
+    constructor() {
+        this._indicator = null;
+        this.label = null;
+        this.timeout = null;
+    }
+
+    updateSpeed() {
+        let proc = Gio.Subprocess.new(
+            ['curl', url],
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+    
+        proc.communicate_utf8_async(null, null, (proc, res) => {
+            try {
+                let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+    
+                if (proc.get_successful()) {
+                    let jsonData = JSON.parse(stdout);
+                    const speed = jsonData.speed;
+
+                    this.label.set_text(`${speed} km/h`);
+                    this.queueUpdate();
+                } else {
+                    throw new Error(stderr);
+                }
+            } catch (e) {
+                logError(e);
+            }
+        });        
+        
+    }
+
+    queueUpdate() {
+        const that = this;
+        this.timeout = Mainloop.timeout_add_seconds(5, () => {
+            that.updateSpeed();
+        });
+    }
+    
+    enable() {
+        log(`enabling ${Me.metadata.name}`);
+
+        let indicatorName = `${Me.metadata.name} Indicator`;
+        this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
+        this.label = new St.Label({style_class: 'iceportal-label', text: `0 km/h`});
+        this._indicator.add_child(this.label);
+        Main.panel.addToStatusArea(indicatorName, this._indicator);
+        this.updateSpeed();
+        this.queueUpdate();
+    }
+    
+    // REMINDER: It's required for extensions to clean up after themselves when
+    // they are disabled. This is required for approval during review!
+    disable() {
+        log(`disabling ${Me.metadata.name}`);
+
+        this._indicator.destroy();
+        this._indicator = null;
+
+        this.label.destroy();
+        this.label = null;
+        this.timeout = null;
+
+        Mainloop.source_remove(timeout);
+    }
+}
+
+function init() {
+    log(`initializing ${Me.metadata.name}`);
+    
+    return new Extension();
+}
